ef467e51c44df8b0bab7a02b4b3ddab66b6888e4
nx673747 <nafi.xhafa@broadcom.com>
2024-09-05 16:38:14 +0200

    update renovate to signoff the commits
    Signed-off-by: nx673747 <nafi.xhafa@broadcom.com>

--- renovate-config.js
+++ renovate-config.js
@@ -1,38 +1,24 @@
 module.exports = {
-    globalExtends: ["config:recommended"], // using this instead of "extends" solves the problem with order of the configuration
-    repositories: ['zowe/api-layer'],
-    baseBranches: ["v2.x.x","v3.x.x"],
+    globalExtends: ["config:recommended", ":gitSignOff"], // using this instead of "extends" solves the problem with order of the configuration
+    repositories: ['nxhafa/api-layer'],
+    baseBranches: ["updatingRenovate"],
     dependencyDashboard: true,
+    includePaths: ["api-catalog-ui/frontend/**"],
     packageRules: [
         {
             //for v2.x.x branch ignore grouping from extends preset, find all packages which are patches,
             // slug them and make PR with name "all patch dependencies"
-            "matchBaseBranches": ["v2.x.x"],
+            "matchBaseBranches": ["updatingRenovate"],
+            "matchPackageNames": ["@emotion/react"],
             "groupName": "all patch dependencies",
             "groupSlug": "all-patch",
-            "matchPackageNames": ["*"],
             "matchUpdateTypes": ["patch"],
         },
+    ],
+    hostRules: [
         {
-            //for v2.x.x make dashboard approval to all major and minor dependencies updates
-            "matchBaseBranches": ["v2.x.x"],
-            "matchUpdateTypes": ["major", "minor"],
-            "dependencyDashboardApproval": true,
-        },
-        {
-            //for v3.x.x branch find all packages which are minor and patches,
-            // slug them and make PR with name "all non-major dependencies"
-            "matchBaseBranches": ["v3.x.x"],
-            "groupName": "all non-major dependencies",
-            "groupSlug": "all-minor-patch",
-            "matchPackageNames": ["*"],
-            "matchUpdateTypes": ["minor", "patch"]
-        },
-        {
-            //for v3.x.x make dashboard approval to all major dependencies updates
-            "matchBaseBranches": ["v3.x.x"],
-            "matchUpdateTypes": ["major"],
-            "dependencyDashboardApproval": true,
+            "hostType": "npm",
+            "matchHost": "https://zowe.jfrog.io/artifactory/api/npm/npm-org/"
         }
     ],
     printConfig: true,
